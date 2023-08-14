f75518d8aaf59d4d1baec684766e5b0d58dd204b
Quypham14 <quykun1422003@gmail.com>
2023-08-14 21:30:58 +0700

    first commit

--- Hit the Lottery_cf.cpp
+++ Hit the Lottery_cf.cpp
@@ -1,34 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	int n;
-	cin>>n;
-	int dem=0;
-	while(n!=0)
+	cin >> n;
+	int dem = 0;
+	while (n != 0)
 	{
-	if(n>=100)
-	{
-		n-=100;
-		dem++;
+		if (n >= 100)
+		{
+			n -= 100;
+			dem++;
+		}
+		else if (n >= 20)
+		{
+			n -= 20;
+			dem++;
+		}
+		else if (n >= 10)
+		{
+			n -= 10;
+			dem++;
+		}
+		else if (n >= 5)
+		{
+			n -= 5;
+			dem++;
+		}
+		else
+		{
+			n -= 1;
+			dem++;
+		}
 	}
-	else if(n>=20)
-	{
-		n-=20;
-		dem++;
-	}
-	else if(n>=10)
-	{
-	n-=10;
-	dem++;
-	}
-	else if(n>=5)
-	{
-		n-=5;
-		dem++;
-	}
-	else{n-=1;
-	dem++;}}
-	cout<<dem;
+	cout << dem;
 	return 0;
 }
